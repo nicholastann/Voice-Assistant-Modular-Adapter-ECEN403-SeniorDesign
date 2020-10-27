078f0348188ae1d29e3fedbe492e6cc7c0c36d06
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-10-26 21:58:59 -0500

    removed cloudfare css

--- appliances.php
+++ appliances.php
@@ -8,7 +8,6 @@ $machine_id = $_GET['appliance_id'];
     <title>On off toggle</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.5.0/flatly/bootstrap.min.css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
