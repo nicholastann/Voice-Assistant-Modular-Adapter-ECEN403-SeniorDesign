8b697389ca62105ea24e49a6f8629ef52b0ffbc1
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-11-01 18:35:58 -0600

    api frame

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+require 'appliances/appliances.php';
+
+$appliances = getappliances();
+?>
---
+++ api/view.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/appliances/appliances.php';
+
+if (!isset($_GET['id'])) {
+    include "../partials/not_found.php";
+    exit;
+}
+$applianceId = $_GET['id'];
+
+$appliance = getapplianceById($applianceId);
+if (!$appliance) {
+    include "../partials/not_found.php";
+    exit;
+}
+
+?>
