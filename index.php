34047ae28057c361203a8a7e085b07d5a3e89ae8
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-11-01 18:23:21 -0600

    total rehaul to crud php app

--- index.php
+++ index.php
@@ -1,6 +1,44 @@
 <?php
+require 'appliances/appliances.php';
 
-require "crud.php";
+$appliances = getappliances();
 
-getAppliances();
+include 'partials/header.php';
 ?>
+
+
+<div class="container">
+    <p>
+        <a class="btn btn-success" href="create.php">Create new appliance</a>
+    </p>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>status</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($appliances as $appliance): ?>
+            <tr>
+                <td><?php echo $appliance['name'] ?></td>
+                <td><?php echo $appliance['status'] ?></td>
+                <td>
+                    <a href="view.php?id=<?php echo $appliance['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
+                    <a href="update.php?id=<?php echo $appliance['id'] ?>"
+                       class="btn btn-sm btn-outline-secondary">Update</a>
+                    <form method="POST" action="delete.php">
+                        <input type="hidden" name="id" value="<?php echo $appliance['id'] ?>">
+                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach;; ?>
+        </tbody>
+    </table>
+</div>
+
+<?php include 'partials/footer.php' ?>
+
---
+++ view.php
@@ -0,0 +1,46 @@
+<?php
+include 'partials/header.php';
+require __DIR__ . '/appliances/appliances.php';
+
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
+$applianceId = $_GET['id'];
+
+$appliance = getapplianceById($applianceId);
+if (!$appliance) {
+    include "partials/not_found.php";
+    exit;
+}
+
+?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>View appliance: <b><?php echo $appliance['name'] ?></b></h3>
+        </div>
+        <div class="card-body">
+            <a class="btn btn-secondary" href="update.php?id=<?php echo $appliance['id'] ?>">Update</a>
+            <form style="display: inline-block" method="POST" action="delete.php">
+                <input type="hidden" name="id" value="<?php echo $appliance['id'] ?>">
+                <button class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+        <table class="table">
+            <tbody>
+            <tr>
+                <th>Name:</th>
+                <td><?php echo $appliance['name'] ?></td>
+            </tr>
+            <tr>
+                <th>status:</th>
+                <td><?php echo $appliance['status'] ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<?php include 'partials/footer.php' ?>
