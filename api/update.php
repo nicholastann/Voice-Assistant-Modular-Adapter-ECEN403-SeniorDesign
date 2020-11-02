ec8dcf21d89fc53694157e2519cada52e8670643
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-11-01 19:03:07 -0600

    update api

---
+++ api/update.php
@@ -0,0 +1,33 @@
+<?php
+require __DIR__ . '../appliances/appliances.php';
+
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
+$applianceId = $_GET['id'];
+
+$appliance = getapplianceById($applianceId);
+if (!$appliance) {
+    include "partials/not_found.php";
+    exit;
+}
+
+$errors = [
+    'name' => "",
+    'status' => ""
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $appliance = array_merge($appliance, $_POST);
+
+    $isValid = validateappliance($appliance, $errors);
+
+    if ($isValid) {
+        $appliance = updateappliance($_POST, $applianceId);
+    }
+}
+
+?>
+
+<?php include '_form.php' ?>
