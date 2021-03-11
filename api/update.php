8d1749f11da91352422885ad3f7644f4ae8c9628
nicholastann <54993961+nicholastann@users.noreply.github.com>
2021-03-10 19:25:23 -0600

    Update update.php

--- api/update.php
+++ api/update.php
@@ -7,23 +7,20 @@
     $applianceStatus = $data_back->{"status"};
     $applianceChannel = $data_back->{"channel"};
     $applianceVolume = $data_back->{"volume"};
-    $applianceUrl = $data_back->{"url"};
 
     $appliance = [
         'id' => $applianceId,
         'name' => $applianceName,
         'status' => $applianceStatus,
         'channel' => $applianceChannel,
-        'volume' => $applianceVolume,
-        'url' => $applianceUrl
+        'volume' => $applianceVolume
     ];
     
     $errors = [
         'name' => "",
         'status' => "",
         'channel' => "",
-        'volume' => "",
-        'url' => ""
+        'volume' => ""
     ];
     
     $appliance = array_merge($appliance, $_POST);
@@ -32,31 +29,7 @@
 
     if ($isValid) {
         $appliance = updateappliance($appliance, $applianceId);
-
-        //The url you wish to send the POST request to
-        $url = $applianceUrl;
-
-        //The data you want to send via POST
-        $fields = json_encode($appliance);
-
-        //url-ify the data for the POST
-        $fields_string = http_build_query($fields);
-
-        //open connection
-        $ch = curl_init();
-
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
-
-        //execute post
-        $result = curl_exec($ch);
         echo $result;
-        
     }
     
 ?>
