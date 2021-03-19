86527a01f3bdcdf0851edb7356c9699ba9fb3a35
nicholastann <54993961+nicholastann@users.noreply.github.com>
2021-03-19 16:21:05 -0500

    adjust validations

--- appliances/appliances.php
+++ appliances/appliances.php
@@ -81,32 +81,33 @@ function validateappliance($appliance, &$errors)
         $errors['name'] = 'Name is mandatory';
     }
     if (!$appliance['status']) {
-        if ((int)$appliance['status'] != '0') {
-            if ((int)$appliance['status'] != '1') {
-                $isValid = false;
-                $errors['status'] = 'Status must be 1 or 0';
-            }
+        $isValid = false;
+        $errors['status'] = 'Status is mandatory';
+    }
+    if ((int)$appliance['status'] != '0') {
+        if ((int)$appliance['status'] != '1') {
+            $isValid = false;
+            $errors['status'] = 'Status must be 1 or 0';
         }
     }
-    if (!(int)$appliance['channel']) {
-        if ((int)$appliance['channel'] < 1) {
-            $isValid = false;
-            $errors['channel'] = 'channel cannot be less than 1';
-        }
-        else if ((int)$appliance['channel'] > 10000) {
-            $isValid = false;
-            $errors['channel'] = 'channel cannot be greater than 10000';
-        }
+
+    if ((int)$appliance['channel'] < 1) {
+        $isValid = false;
+        $errors['channel'] = 'channel cannot be less than 1';
     }
-    if (!(int)$appliance['volume']) {
-        if ((int)$appliance['volume'] < 0) {
-            $isValid = false;
-            $errors['volume'] = 'volume cannot be less than 0';
-        }
-        else if ((int)$appliance['volume'] > 100) {
-            $isValid = false;
-            $errors['volume'] = 'volume cannot be greater than 100';
-        }
+    else if ((int)$appliance['channel'] > 10000) {
+        $isValid = false;
+        $errors['channel'] = 'channel cannot be greater than 10000';
+    }
+
+
+    if ((int)$appliance['volume'] < 0) {
+        $isValid = false;
+        $errors['volume'] = 'volume cannot be less than 0';
+    }
+    else if ((int)$appliance['volume'] > 100) {
+        $isValid = false;
+        $errors['volume'] = 'volume cannot be greater than 100';
     }
     // End Of validation
 
