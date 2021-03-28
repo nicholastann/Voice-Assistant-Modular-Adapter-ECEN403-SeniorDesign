853f98a766527270323f3fb1d8f2b40a0c72228a
nicholastann <54993961+nicholastann@users.noreply.github.com>
2021-03-27 19:16:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -93,10 +93,23 @@ include 'partials/header.php';
                 <?php if ($appliance["type"] === "switch"): ?>
                     <tr>
                         <td>
-                            <form action="/action_page.php">
-                                <input type="file" id="myFile" name="filename">
-                                <input type="submit">
+                        <form action="fileupload.php" enctype="multipart/form-data" method="post">
+                            Select image :
+                            <input type="file" name="file"><br/>
+                            <input type="submit" value="Upload" name="Submit1"> <br/>
                             </form>
+                            <?php
+                                if(isset($_POST['Submit1'])) { 
+                                    $filepath = "images/" . $_FILES["file"]["name"];
+
+                                    if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) {
+                                        echo "<img src=".$filepath." height=200 width=300 />";
+                                    } 
+                                    else {
+                                        echo "Error !!";
+                                    }
+                                } 
+                            ?>
                         </td>
                         <td><?php echo $appliance['id'] ?></td>
                         <td><?php echo $appliance['name'] ?></td>
