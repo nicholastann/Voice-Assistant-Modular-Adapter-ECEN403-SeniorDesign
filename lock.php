47017b4c4365ea84a1e2f0bb6c07ee3587f92e9a
nicholastann <54993961+nicholastann@users.noreply.github.com>
2021-04-09 14:12:05 -0500

    Update lock.php

--- lock.php
+++ lock.php
@@ -49,8 +49,8 @@ include 'partials/header.php';
 </body>
 <nav class="navbar">
   <a href="index.php"><i class="fa fa-fw fa-home"></i></a>
-  <a class="active" href=""><i class="fa fa-lock"></i></a>
   <a href="light.php"><i class="fa fa-lightbulb-o"></i></a>
+  <a class="active" href=""><i class="fa fa-lock"></i></a>
   <a href="tv.php"><i class="fa fa-tv"></i></a>
 </nav>
 
