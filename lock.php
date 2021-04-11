2ff98f55b61f57a8c49811b031ae915a519def0a
nicholastann <54993961+nicholastann@users.noreply.github.com>
2021-04-11 18:21:15 -0500

    rm form

--- lock.php
+++ lock.php
@@ -57,10 +57,3 @@ include 'partials/header.php';
 <?php include 'partials/footer.php' ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
-
-
-<style type="text/css">
-form {
-    display: inline;
-}
-</style>
